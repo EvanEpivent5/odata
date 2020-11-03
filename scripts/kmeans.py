a31976db1e7d18bec3cda5daaca8e3b097a866d4
EvanEpivent <evan.epivent@hotmail.fr>
2020-11-03 14:25:23 +0100

    test

--- scripts/kmeans.py
+++ scripts/kmeans.py
@@ -1,2 +1,17 @@
-a=1
-b=2
+from sklearn.cluster import KMeans
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data=pd.read_csv('src/data.csv',sep=',')
+
+
+corr=data.corr()
+#print(corr)
+
+a=pd.DataFrame(pd.read_csv('src/g2-2-20.txt'))
+print(type(a))
+
+pd.DataFrame.plot(a)
+
+plt.plot([[1,1],[2,3]])
+plt.savefig('img.png')
